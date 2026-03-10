6f6968391b367d75d51a17f59ff6efa3cd935b27
H1ppoG <zoloto0007@gmail.com>
2026-03-10 21:31:26 +0300

    packege

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+// отдаем все файлы из текущей папки
+app.use(express.static(path.join(__dirname)));
+
+// по умолчанию – index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
